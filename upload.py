af2b1af4c38a29cb89713ce30b19d1f6d51d19f0
Sahil Rai <sahil8rai@gmail.com>
2023-09-27 09:50:24 +0530

    "commit"

--- upload.py
+++ upload.py
@@ -2,8 +2,14 @@ import os
 import subprocess
 
 run_cmd = lambda x:subprocess.Popen(x)
+# os.
 
-subprocess.Popen(['landslide','-c','-r','test.md','-d','index.html'])
-subprocess.Popen(['git','add','.'])
-subprocess.Popen(['git','commit','-a','-m','"commit"'])
-subprocess.Popen(['git','push'])
+subprocess.Popen(['landslide','-c','-r','test.md','-d','index.html']).communicate()
+subprocess.Popen(['git','add','.']).communicate()
+subprocess.Popen(['git','commit','-a','-m','"commit"']).communicate()
+subprocess.Popen(['git','push']).communicate()
+print('Updated')
+# subprocess.Popen(['landslide','-c','-r','test.md','-d','index.html'],stdout=subprocess.PIPE)
+# subprocess.Popen(['git','add','.'],stdout=subprocess.PIPE)
+# subprocess.Popen(['git','commit','-a','-m','"commit"'],stdout=subprocess.PIPE)
+# subprocess.Popen(['git','push'],stdout=subprocess.PIPE)
